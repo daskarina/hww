89c2d7ed5c693b60dc94496e8678f3721dbc1299
user <veselkarina6@gmail.com>
2024-05-21 18:12:41 +0300

    my first commit

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
-from colorama import just_fix_windows_console
-just_fix_windows_console()
+result = []
+def divider(a, b):
+    try:
+        if a < b:
+            raise ValueError("ValueError: 'a' should be greater than or equal to 'b'")
+        if b == 0:
+            raise ZeroDivisionError("ZeroDivisionError: division by zero")
+        if b > 100:
+            raise IndexError("IndexError: 'b' should be less than or equal to 100")
+        return a / b
+    except ValueError as ve:
+        print(ve)
+    except ZeroDivisionError as zde:
+        print(zde)
+    except IndexError as ie:
+        print(ie)
 
-from colorama import init
-init()
-
-from colorama import Fore, Back, Style
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(Style.RESET_ALL)
-print('back to normal now')
-
-print('\033[31m' + 'some red text')
-print('\033[39m') # and reset to default color
-
-from colorama import just_fix_windows_console
-from termcolor import colored
-
-print(colored('Hello, World!', 'green', 'on_red')
-
-Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
-Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
-Style: DIM, NORMAL, BRIGHT, RESET_ALL
-
-from colorama import init
-init(autoreset=True)
-print(Fore.RED + 'some red text')
-print('automatically back to default color again')
-
-import sys
-from colorama import init, AnsiToWin32
-init(wrap=False)
-stream = AnsiToWin32(sys.stderr).stream
-
-# Python 2
-print >>stream, Fore.BLUE + 'blue text on stderr'
-
-# Python 3
-print(Fore.BLUE + 'blue text on stderr', file=stream)
-
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+for key in data:
+    res = divider(key, data[key])
+    result.append(res)
